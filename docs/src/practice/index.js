d5bc9ba5de11be611b1b379242db0c2f11eb40cc
张喜硕 <zhangxishuo1998@outlook.com>
2022-04-30 22:11:13 +0800

    init

---
+++ docs/src/practice/index.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  return [
+    '',
+    {
+      title: '道德经',
+      path: '/practice/tao/',
+      collapsable: false,
+      children: [
+        'tao/01.md'
+      ]
+    }
+  ]
+}
