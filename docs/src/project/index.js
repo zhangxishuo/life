05aa05f0e0e426a6772e5be1e0127254cb2a2aa7
张喜硕 <zhangxishuo1998@outlook.com>
2022-05-02 22:46:01 +0800

    调整结构

--- docs/src/project/index.js
+++ docs/src/project/index.js
@@ -2,10 +2,26 @@ module.exports = () => {
   return [
     '',
     {
-      title: 'TensorFlow',
-      path: '/project/tensorflow/',
+      title: '编译原理',
+      path: '/project/compile/',
       collapsable: false,
       children: [
+        'compile/01.lexical-analysis.md',
+        'compile/02.syntactic-analysis.md',
+        'compile/03.semantic-analysis.md',
+        'compile/04.runtime.md',
+        'compile/05.intermediate-representation.md',
+        'compile/06.optimization.md',
+        'compile/07.generation.md',
+        'compile/08.java-compiler.md'
+      ]
+    },
+    {
+      title: '深入理解Java虚拟机',
+      path: '/project/jvm',
+      collapsable: false,
+      children: [
+        'jvm/01.basic-type.md'
       ]
     }
   ]
--- docs/src/science/index.js
+++ docs/src/science/index.js
@@ -2,26 +2,10 @@ module.exports = () => {
   return [
     '',
     {
-      title: '编译原理',
-      path: '/science/compile/',
+      title: '人工智能',
+      path: '/science/ai/',
       collapsable: false,
       children: [
-        'compile/01.lexical-analysis.md',
-        'compile/02.syntactic-analysis.md',
-        'compile/03.semantic-analysis.md',
-        'compile/04.runtime.md',
-        'compile/05.intermediate-representation.md',
-        'compile/06.optimization.md',
-        'compile/07.generation.md',
-        'compile/08.java-compiler.md'
-      ]
-    },
-    {
-      title: '深入理解Java虚拟机',
-      path: '/science/jvm',
-      collapsable: false,
-      children: [
-        'jvm/01.basic-type.md'
       ]
     }
   ]
